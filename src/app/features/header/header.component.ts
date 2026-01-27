826abd9f846e10bf53ae272893997efb7a2f6dc9
Sergey Malyuga <sergey@malyuga.ru>
2026-01-27 15:54:02 +0300

    Создать toggle.directive. Реализовать открытие/закрытие navigation-overlay при нажатии на button burger

--- src/app/features/header/header.component.ts
+++ src/app/features/header/header.component.ts
@@ -1,15 +1,21 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import {ChangeDetectionStrategy, Component, signal} from '@angular/core';
 import {MatBadge} from '@angular/material/badge';
+import {ToggleDirective} from '../../shared/directives/toggle.directive';
 
 @Component({
   selector: 'app-header',
   imports: [
     MatBadge,
+    ToggleDirective,
   ],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class HeaderComponent {
+  public isNavOpen = signal<boolean>(false);
 
+  public toggleNav() {
+    this.isNavOpen.set(!this.isNavOpen());
+  }
 }
---
+++ src/app/shared/directives/toggle.directive.ts
@@ -0,0 +1,13 @@
+import {Directive, EventEmitter, HostListener, Output} from '@angular/core';
+
+@Directive({
+  selector: '[appToggle]',
+})
+export class ToggleDirective {
+  @Output() toggled = new EventEmitter<void>();
+
+  @HostListener('click')
+  onClick() {
+    this.toggled.emit();
+  }
+}
