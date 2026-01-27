aeee6c1277638f80fe62e1f162d715101a6f0d75
Sergey Malyuga <sergey@malyuga.ru>
2026-01-27 10:56:54 +0300

    Подключить к проекту angular material

--- src/app/features/header/header.component.ts
+++ src/app/features/header/header.component.ts
@@ -1,8 +1,11 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import {MatBadge} from '@angular/material/badge';
 
 @Component({
   selector: 'app-header',
-  imports: [],
+  imports: [
+    MatBadge,
+  ],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush
