17a6851509be265d902352c5fd2663dbac0ada8e
Sergey Malyuga <sergey@malyuga.ru>
2026-02-02 13:29:08 +0300

    Закончить вёрстку hero__wrapper-column-left

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,7 +3,7 @@ import { Routes } from '@angular/router';
 export const routes: Routes = [
   {
     path: '',
-    title: 'Mail',
+    title: 'Moon River',
     loadComponent: () =>
       import('./pages/main/main.component').then((m) => m.MainComponent),
   },
