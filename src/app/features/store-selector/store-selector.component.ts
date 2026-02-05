49378104d89e934fdd56c66746b7ba28c77934e9
Sergey Malyuga <sergey@malyuga.ru>
2026-02-05 11:12:26 +0300

    Создать store-selector.component

---
+++ src/app/features/store-selector/store-selector.component.ts
@@ -0,0 +1,12 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-store-selector',
+  imports: [],
+  templateUrl: './store-selector.component.html',
+  styleUrl: './store-selector.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class StoreSelectorComponent {
+
+}
---
+++ src/app/shared/map/map.component.ts
@@ -0,0 +1,12 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-map',
+  imports: [],
+  templateUrl: './map.component.html',
+  styleUrl: './map.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class MapComponent {
+
+}
