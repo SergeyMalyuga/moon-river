ed85f2ce3b8f838ee53da3b678a268d5c8b8b7a0
Sergey Malyuga <sergey@malyuga.ru>
2026-02-11 09:56:57 +0300

    Исправить ошибку с тенью маркера для map

--- src/app/shared/map/map.component.ts
+++ src/app/shared/map/map.component.ts
@@ -1,25 +1,30 @@
-import {Component, OnInit} from '@angular/core';
+import {AfterViewInit, Component, OnInit} from '@angular/core';
 import * as L from 'leaflet';
 
 @Component({
   selector: 'app-map',
   templateUrl: './map.component.html',
 })
-export class MapComponent implements OnInit {
+export class MapComponent implements AfterViewInit {
   private map!: L.Map;
   private centroid: L.LatLngExpression = [55.7610, 37.6095];
   private defaultIcon = new L.Icon({
     iconUrl: '../assets/images/vector/map-marker.svg',
     iconSize: [44, 60],
     iconAnchor: [22, 60],
+    shadowUrl: 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAADUlEQVR42mNk+P+/HgAFhAJ/wlseKgAAAABJRU5ErkJggg==',
+    shadowSize: [0, 0],
+
   })
   private hoverIcon = new L.Icon({
     iconUrl: '../assets/images/vector/map-marker-hover.svg',
     iconSize: [44, 60],
     iconAnchor: [22, 60],
+    shadowUrl: 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAADUlEQVR42mNk+P+/HgAFhAJ/wlseKgAAAABJRU5ErkJggg==',
+    shadowSize: [0, 0],
   })
 
-  ngOnInit(): void {
+  ngAfterViewInit(): void {
     this.initMap();
   }
 
